367f24b767ff8b3042f69a8ae2d466e3e4430644
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-08-11 19:18:22 +0200

    Prettier Configuration – Sometimes It Wants to Work, Sometimes It Doesn’t

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-center md:text-right">Holis App</h1>
+  return <h1 className="bg-purple-600 text-center md:text-right">Holis App</h1>;
 }
 
-export default App
+export default App;
